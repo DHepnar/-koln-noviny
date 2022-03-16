0e0a935cb11b384e4b02bf5b4a1e71794e3bd563
Dominik Hepnar <hepnar@spstrutnov.cz>
2022-03-16 19:53:17 +0100

    Controller a router pro příspěvek

---
+++ src/app/controllers/prispevekController.js
@@ -0,0 +1,3 @@
+exports.zobrazDetail = (dotaz, odpoved) => {
+    
+};
---
+++ src/app/routers/prispevekRouter.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const controller = require(path.join(__dirname, '..', 'controllers', 'prispevekController.js'))
+
+router.get('/detail', )
+
+module.exports = router;
