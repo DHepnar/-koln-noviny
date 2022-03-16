23a06177cd82e6b6b3363568e4b19437a54f64d0
Dominik Hepnar <hepnar@spstrutnov.cz>
2022-03-16 22:37:35 +0100

    adminRouter, adminController a možnost jít z přehledu na admin page

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,14 @@ const express = require('express');
 const app = express();
 
 const prispevekRouter = require(path.join(__dirname, 'routers', 'prispevekRouter.js'));
+const adminRouter = require(path.join(__dirname, 'routers', 'adminRouter.js'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use('/admin', adminRouter);
 app.use('/prispevky', prispevekRouter);
 
 module.exports = app;
